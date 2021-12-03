314896a23402322ecad7832fb06660674a659039
dionriadi <riadidion@gmail.com>
2021-12-03 16:50:10 +0700

    membuat halaman user

---
+++ includes/script.php
@@ -0,0 +1,7 @@
+<script src="admin/assets/vendors/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+<script src="admin/assets/js/bootstrap.bundle.min.js"></script>
+    
+<script src="admin/assets/vendors/apexcharts/apexcharts.js"></script>
+<script src="admin/assets/js/pages/dashboard.js"></script>
+
+<script src="admin/assets/js/pages/horizontal-layout.js"></script>
---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<!-- head -->
+<head>
+<?php include("includes/head.php"); ?>
+</head>
+<!-- head -->
+
+<body>
+    <div id="app">
+        <div id="main" class="layout-horizontal">
+            <!-- header -->
+            <header class="mb-5">
+            <?php include("includes/header.php");?>
+                <!-- navigasi -->
+                <?php include("includes/sidebar.php"); ?>
+                <!-- navigasi -->
+
+            </header>
+            <!-- header -->
+
+            <!-- isi -->
+            <div class="content-wrapper container">
+                
+            </div>
+
+            <footer>
+            <?php include("includes/footer.php"); ?>
+            </footer>
+        </div>
+    </div>
+    <!-- script -->
+    <?php include("includes/script.php"); ?>
+    <!-- script -->
+</body>
+
+</html>
