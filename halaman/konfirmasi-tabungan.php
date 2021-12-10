a4eea5cc9a0997199a4a1159219917a0289926f7
dionriadi <riadidion@gmail.com>
2021-12-10 09:21:58 +0700

    tambah menu tabungan

---
+++ halaman/konfirmasi-tabungan.php
@@ -0,0 +1,29 @@
+<?php
+      //mengambil input user dari form
+      // include("../koneksi/koneksi.php");
+        $jenis = $_POST['jenis'];
+        $nominal = $_POST['nominal'];
+        $metode = $_POST['metode'];
+        $id_anggota=$_SESSION['id_user'];
+        $jenis_bayar = "Tabungan";
+        $tgl = date("Y-m-d");
+      //mengambil data dari database
+      if(empty($jenis)){
+	    header("Location:tabungan_kosong_jenis");
+        }else if(empty($nominal)){
+            header("Location:tabungan_kosong_nominal");
+        }else if(empty($metode)){
+            header("Location:tabungan_kosong_metode");
+        }else{    
+          $sql = "INSERT INTO simpanan (id_anggota,id_jenis_simpanan, id_metode, jumlah) 
+                  values ('$id_anggota','$jenis','$metode', '$nominal')";
+                  mysqli_query($koneksi,$sql);
+
+          $sql_t = "INSERT INTO transaksi (id_anggota,jenis_pembayaran, tgl_transaksi, jumlah_pembayaran) 
+                  values ('$id_anggota','$jenis_bayar','$tgl', '$nominal')";
+                  mysqli_query($koneksi,$sql_t);
+        header("location:tabung-berhasil");	
+        }
+            
+    
+?>
